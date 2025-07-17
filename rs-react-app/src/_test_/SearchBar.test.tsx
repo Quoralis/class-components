04bc64eff87c46aa054d6e32aeb675c52f04d324
StanislavDevel <unfeel00@gmail.com>
2025-07-17 18:45:35 +0200

    test: add tests for SearchBar - input typing and click button

--- rs-react-app/src/_test_/SearchBar.test.tsx
+++ rs-react-app/src/_test_/SearchBar.test.tsx
@@ -1,18 +1,38 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { SearchBar } from '../components/SearchBar.tsx';
+import userEvent from '@testing-library/user-event';
 
 describe('Test SearchBar', () => {
   const inputValue = '';
   let input: HTMLInputElement;
+  let buttonSearch: HTMLButtonElement;
+  const user = userEvent.setup();
+  const onSearchMok = vi.fn();
+
   beforeEach(() => {
-    render(<SearchBar onSearch={vi.fn()} />);
+    render(<SearchBar onSearch={onSearchMok} />);
     input = screen.getByRole('textbox');
+    buttonSearch = screen.getByRole('button', { name: 'Search' });
   });
-  test('Test empty input', () => {
+
+  test('renders empty input initially', () => {
     expect(input.value).toBe(inputValue);
   });
+
   test('renders input field', () => {
     expect(input).toBeInTheDocument();
+  });
+
+  test('updates input value on user typing', async () => {
+    await user.type(input, 'Qwerty');
+    expect(input).toHaveValue('Qwerty');
+  });
+
+  test('test button is clicked ', async () => {
+    await user.type(input, 'Qwerty');
+    await user.click(buttonSearch);
+
+    expect(onSearchMok).toHaveBeenCalledWith('Qwerty');
   });
 });
