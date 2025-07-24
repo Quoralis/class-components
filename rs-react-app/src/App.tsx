4c1e22f1074c8d261f48ac8ea2df5fefc6b4dc5b
StanislavDevel <unfeel00@gmail.com>
2025-07-24 17:33:33 +0200

    feat: add layout page 404 and about  page

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,6 +1,8 @@
 import NavMenu from './components/NavMenu.tsx';
 import { Route, Routes } from 'react-router-dom';
 import SearchPage from './components/pages/SearchResults';
+import Page404 from './components/pages/Page404/Page404.tsx';
+import AboutPage from './components/pages/About/AboutPage.tsx';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <NavMenu />
       <Routes>
         <Route path="/" element={<SearchPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="*" element={<Page404 />}></Route>
       </Routes>
     </div>
   );
---
+++ rs-react-app/src/components/pages/About/AboutPage.tsx
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <div>Something about page</div>;
+}
+
+export default AboutPage;
