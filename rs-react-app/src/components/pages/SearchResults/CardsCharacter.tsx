b0630350c19aadf5215273f89fecf2aa5c95e991
StanislavDevel <unfeel00@gmail.com>
2025-07-27 20:49:37 +0200

    feat: add close button and preserve page query param during navigation

--- rs-react-app/src/components/pages/SearchResults/CardsCharacter.tsx
+++ rs-react-app/src/components/pages/SearchResults/CardsCharacter.tsx
@@ -1,18 +1,23 @@
 // src/components/pages/SearchResults/CardCharacter.tsx
 import type { Item } from './ResultsFieldFn';
 import styles from './CardCharacter.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 interface Props {
   items: Item[];
 }
 
 export default function CardCharacter({ items }: Props) {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.toString();
   return (
     <ul className={styles['card-list']}>
       {items.map((item) => (
         <li key={item.uid} className={styles['card']}>
-          <Link to={`/details/id=${item.uid}`} className={styles['card-link']}>
+          <Link
+            to={`/details/${item.uid}${query ? `?${query}` : ''}`}
+            className={styles['card-link']}
+          >
             <h3>{item.name}</h3>
             <table>
               <tbody>
--- rs-react-app/src/components/pages/SearchResults/ItemDetails.tsx
+++ rs-react-app/src/components/pages/SearchResults/ItemDetails.tsx
@@ -1,5 +1,15 @@
-function ItemDetails() {
-  return <div>Details Items</div>;
-}
+import { useSearchParams, Link } from 'react-router-dom';
 
-export default ItemDetails;
+export default function ItemDetails() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.toString();
+
+  return (
+    <div>
+      <h2>Details</h2>
+      <Link to={`/?${query}`}>
+        <button>X</button>
+      </Link>
+    </div>
+  );
+}
