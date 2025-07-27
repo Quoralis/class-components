98a1cc04200e5fd8c893d239ddfba442a343f04f
StanislavDevel <unfeel00@gmail.com>
2025-07-27 22:19:35 +0200

    feat: add new hook - useDataCharacterByUid and use it  in item ItemDetails

--- rs-react-app/src/components/pages/SearchResults/ItemDetails.tsx
+++ rs-react-app/src/components/pages/SearchResults/ItemDetails.tsx
@@ -1,14 +1,42 @@
-import { useSearchParams, Link } from 'react-router-dom';
+import { useParams, useSearchParams, Link } from 'react-router-dom';
+import useCharacterById from '../../../hooks/useDataCharacterByUid.ts';
 
 export default function ItemDetails() {
+  const { id } = useParams();
   const [searchParams] = useSearchParams();
   const query = searchParams.toString();
 
+  const { character, loading, error } = useCharacterById(id ?? '');
+  if (!id) {
+    return <div>Missing UID</div>;
+  }
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!character)
+    return (
+      <div>
+        <Link to={`/?${query}`}>
+          <button>X</button>
+        </Link>
+        Character not found
+      </div>
+    );
+
   return (
     <div>
-      <h2>Details</h2>
+      <h2>{character.name}</h2>
+      <p>
+        <strong>UID:</strong> {character.uid || 'unknown'}
+      </p>
+      <p>
+        <strong>Species:</strong> {character.species || 'unknown'}
+      </p>
+      <p>
+        <strong>Home World:</strong> {character.homeWorld || 'unknown'}
+      </p>
+
       <Link to={`/?${query}`}>
-        <button>X</button>
+        <button>⨯ Back</button>
       </Link>
     </div>
   );
---
+++ rs-react-app/src/hooks/useDataCharacterByUid.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import type { Item } from '../components/pages/SearchResults/ResultsFieldFn';
+
+interface CharacterByIdResponse {
+  character: Item;
+}
+
+export default function useCharacterById(uid: string) {
+  const [character, setCharacter] = useState<Item | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const res = await fetch(
+          `https://stapi.co/api/v1/rest/character?uid=${uid}`
+        );
+        const result: CharacterByIdResponse = await res.json();
+        setCharacter(result.character);
+      } catch (err) {
+        console.error(err);
+        setError('Failed to load character');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (uid) fetchCharacter();
+  }, [uid]);
+
+  return { character, loading, error };
+}
