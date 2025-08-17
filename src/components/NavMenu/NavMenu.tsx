f40caf1b94786bbcdf02421ebc1cb13b7cdfab44
StanislavDevel <unfeel00@gmail.com>
2025-08-17 20:05:13 +0200

    feat: add language switcher, add styles for custom select

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,30 +1,26 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { Link } from '@/i18n/navigation';
 import styles from './NavMenu.module.scss';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
+import LocaleSwitcher from '../LocaleSwitcher/LocaleSwitcher';
+import { routing } from '../../i18n/routing';
 
 export function NavMenu() {
-  const pathname = usePathname();
   const t = useTranslations('Nav');
+  const locale = useLocale();
   return (
     <nav className={styles.navbar}>
-      <Link href="/" className={pathname === '/' ? styles.active : undefined}>
-        {t('main')}
-      </Link>
-      <Link
-        href="/results"
-        className={pathname.startsWith('/results') ? styles.active : undefined}
-      >
-        {t('search')}
-      </Link>
-      <Link
-        href="/about"
-        className={pathname === '/about' ? styles.active : undefined}
-      >
-        {t('about')}
-      </Link>
+      <Link href="/">{t('main')}</Link>
+      <Link href="/results">{t('search')}</Link>
+      <Link href="/about">{t('about')}</Link>
+      <LocaleSwitcher defaultValue={locale} label={t('label')}>
+        {routing.locales.map((cur) => (
+          <option key={cur} value={cur}>
+            {t('locale', { locale: cur })}
+          </option>
+        ))}
+      </LocaleSwitcher>
     </nav>
   );
 }
