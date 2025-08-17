032378de7782ff4a91f1125c68cc9dbca84603e7
StanislavDevel <unfeel00@gmail.com>
2025-08-17 15:41:15 +0200

    feat(ui): show/hide selection bar when cards are selected and add stiles

--- src/components/SelectionBar/SelectionBar.tsx
+++ src/components/SelectionBar/SelectionBar.tsx
@@ -2,12 +2,24 @@
 import styles from './SelectionBar.module.scss';
 import ClearSelectionButton from '../Buttons/ClearSelectionButton';
 import DownloadCsvButton from '../Buttons/DownloadCsvButton';
+import { useSelector } from 'react-redux';
+import type { RootState } from '../../store/store.ts';
 
 export default function SelectionBar() {
+  const selectedCards = useSelector((state: RootState) => state.selector.cards);
+
   return (
-    <div className={styles.results__wrapper}>
+    <div
+      className={
+        selectedCards.length > 0
+          ? `${styles.results__wrapper}`
+          : `${styles.display_none}`
+      }
+    >
       <ClearSelectionButton />
-      <span>Кол-во выделенных элементов</span>
+      <span className={styles.selected_count}>
+        {`${selectedCards.length > 0 ? selectedCards.length : ''} item(s) are selected`}{' '}
+      </span>
       <DownloadCsvButton />
     </div>
   );
