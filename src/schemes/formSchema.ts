1186060169f6eac010cb1b899c5b0926fe2f21ea
StanislavDevel <unfeel00@gmail.com>
2025-08-21 00:02:07 +0200

    feat: add validation  name and  password

--- src/schemes/formSchema.ts
+++ src/schemes/formSchema.ts
@@ -1,11 +1,27 @@
 import { z } from 'zod';
 
+const passwordCheck =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
+
 export const formSchema = z
   .object({
-    name: z.string().min(1, { message: 'Please enter login' }).max(10),
+    name: z
+      .string()
+      .min(1, { message: 'Please enter login' })
+      .max(10)
+      .refine((val) => /^[A-Z]/.test(val), {
+        message: 'First letter must be uppercase',
+      }),
     email: z.email('Please enter a valid email address'),
     age: z.number().min(4, { message: 'Please enter a valid age number' }),
-    password: z.string().min(4, { message: 'Please enter password' }).max(100),
+    password: z
+      .string()
+      .min(4, { message: 'Please enter password' })
+      .max(100)
+      .refine((val) => passwordCheck.test(val), {
+        message:
+          'Password must include uppercase, lowercase, number, and special character',
+      }),
     confirmPassword: z.string(),
     gender: z.string().min(1, { message: 'Please choose gender' }).max(10),
     country: z.string().min(1, { message: 'Please enter a country' }).max(100),
